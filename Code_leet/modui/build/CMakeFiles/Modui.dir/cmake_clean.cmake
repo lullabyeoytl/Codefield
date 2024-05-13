cd338e69dd45e31385f4ed90d1c8cc6c2714544b
lullabyeoytl <2199288762@qq.com>
2024-05-13 21:29:41 +0800

    update

---
+++ Code_leet/modui/build/CMakeFiles/Modui.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Modui.dir/luogu_1494.cc.obj"
+  "CMakeFiles/Modui.dir/luogu_1494.cc.obj.d"
+  "Modui.exe"
+  "Modui.exe.manifest"
+  "Modui.pdb"
+  "libModui.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Modui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
