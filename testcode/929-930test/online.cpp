d6b932d042fd50aa234b58a2a0ea43f4c72080fb
lullabyeoytl <2199288762@qq.com>
2024-09-30 09:42:35 +0800

    matrix fastpow

--- testcode/929-930test/online.cpp
+++ testcode/929-930test/online.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <cstring>
@@ -33,25 +34,56 @@ inline long long lread()
         x=x*10+ch-'0',ch=getchar();
     return x*f;
 }
+
+struct Matrix{
+    long long a[2][2];
+}A;
+
+Matrix operator * (const Matrix& a, const Matrix& b) {
+    Matrix c;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            c.a[i][j] = (a.a[i][0] * b.a[0][j]%MOD + a.a[i][1] * b.a[1][j]%MOD+MOD) % MOD;
+        }
+    }
+    return c;
+}
+
+Matrix fastpow(Matrix a, long long b){
+    Matrix res;
+    res.a[0][0]=1;
+    res.a[0][1]=0;
+    res.a[1][0]=0;
+    res.a[1][1]=1;
+    if(b==1)
+        return a;
+    while(b>0){
+        if(b&1){
+            res =res*a;
+        }
+        a=a*a;
+        b>>=1;
+    }
+    return res;
+}
 int main() {
     int n=read();
     long long num[100001];
     long long max_num = 0;
+    A.a[0][0]=3;
+    A.a[0][1]=-1;
+    A.a[1][0]=1;
+    A.a[1][1]=0;
     for (int i = 0; i < n; i++) {
         num[i]=lread();
         if (num[i] > max_num) {
             max_num = num[i];
         }
+        Matrix tmp = fastpow(A,num[i]-1);
+        long long ans = (tmp.a[0][0]%MOD);
+        printf("%lld\n",ans);
     }
-    long long* dp = (long long*)malloc(sizeof(long long)*(max_num+1)); 
-    dp[0] = 0;
-    dp[1] = 1;
-    for (int i = 2; i <= max_num; i++) {
-        dp[i] = ((3*dp[i-1]-dp[i-2])%MOD+MOD)%MOD;
-    }
-    for(int i=0;i<n;i++){
-        printf("%lld\n",dp[num[i]]);
-    }
-    free(dp);
     return 0;
 }
+
+/**矩阵快速幂 */
